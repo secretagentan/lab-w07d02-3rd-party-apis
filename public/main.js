ca6b62c61300089df6835a9d2e9eb7906fe97afa
an nguyen <hello.an.ng@gmail.com>
2017-01-24 18:24:36 -0800

    saves work

--- public/main.js
+++ public/main.js
@@ -1,1 +1,18 @@
 console.log('hello meow');
+
+const $wordBtn = $('#word');
+const $userBtn = $('#user');
+const $newWord= $('.new-word');
+const $userDiv = $('.new-user');
+
+$wordBtn.on('click', function(evt) {
+  $.post('/randomword', function(res) {
+    // console.log(res);
+    $newWord.text(res);
+    // getData(res);
+  })
+})
+
+// function getData(data) {
+//   $newWord.text(data);
+// }
